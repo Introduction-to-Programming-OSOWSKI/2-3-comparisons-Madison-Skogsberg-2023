41591171cb43498f89757b1ff70a1d2856f08752
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-28 09:17:57 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function
+#add parameters
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+#define function
+#add parameters
+def lessThan(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+#define function
+#add parameters
+def equalTo(x, y):
+    if x == y:
+        return True
+    else:
+        return False
+#define function
+#add parameters
+def greaterOrEqual(x, y):
+    if x > y:
+        return True
+    elif x == y:
+        return True
+    else:
+        return False
+#define function
+#add parameters
+def lessOrEqual(x, y):
+    if x < y:
+        return True
+    elif x == y:
+        return True
+    else:
+        return False
+#run all functions
+print(greaterThan(10, 11))
+print(lessThan(10, 11))
+print(equalTo(10,11))
+print(greaterOrEqual(10,11))
+print(lessOrEqual(10, 11))
